3c8de70ead4be0cd1661ab32aa0bd6a46b239cf9
kenji tachibana <kenji_tachibana@MBR14030.local>
2016-11-05 22:47:13 +0900

    daikadai2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,8 @@ config.action_mailer.default_url_options = { host: 'https://sheltered-ravine-962
   ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
 {
- user_name: ENV[SENDGRID_USERNAME],
- password: ENV[SENDGRID_PASSWORD],
+ user_name: ENV['SENDGRID_USERNAME'],
+ password: ENV['SENDGRID_PASSWORD'],
  domain: "heroku.com",
  address: "smtp.sendgrid.net",
  port: 587,
