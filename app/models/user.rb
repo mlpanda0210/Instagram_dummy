4eab9a5da43693f1f3e2a5f44835a6e83789e06a
tachiken0210 <kenji_tachibana@MBR14030.local>
2016-11-13 19:35:04 +0900

    daikadai2 shuusei 161113

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
          end
 
 def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-  user = User.find_by(provider: auth.provider, uid: auth.uid)
+ user = User.find_by(email: auth.info.email)
 
   unless user
     user = User.new(
